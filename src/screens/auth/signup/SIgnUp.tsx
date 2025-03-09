0c7c5b8e29a5e934454b58cfdf57275967111166
Tuhin810 <tuhin.thakur1233@gmail.com>
2025-03-09 23:17:10 +0530

    Add login and signup components with Google OAuth integration

---
+++ src/screens/auth/signup/SIgnUp.tsx
@@ -0,0 +1,11 @@
+import GoggleLogin from "../../../components/shared/googleLogin/GoggleLogin";
+
+const SIgnUp = () => {
+  return (
+    <div>
+      <GoggleLogin />
+    </div>
+  );
+};
+
+export default SIgnUp;
